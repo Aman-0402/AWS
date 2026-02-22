bcbeb60701bea664304099efe46e438a1dd3a6d2
Aman <aman08.stars@gmail.com>
2026-02-22 23:44:38 +0530

    update

---
+++ assets/js/template.js
@@ -0,0 +1,6 @@
+// Smooth scroll for sidebar links
+document.querySelectorAll('.sidebar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    console.log("Navigating...");
+  });
+});
